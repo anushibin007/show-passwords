0ca7a77717b8ece2d6e8734d65dca1c958be5c10
m-divya-29 <divya.gracie@gmail.com>
2025-05-23 21:29:47 +0530

    :sparkles: toggle extension icon based on state

--- src/background.js
+++ src/background.js
@@ -1,6 +1,38 @@
 chrome.action.onClicked.addListener((tab) => {
-	chrome.scripting.executeScript({
-		target: { tabId: tab.id },
-		files: ["content.js"],
-	});
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ["content.js"],
+  });
+});
+
+chrome.runtime.onMessage.addListener((message, sender) => {
+  console.log("Received message:", message);
+
+  if (message.action === "updateIcon") {
+    // const iconPath = message.showing ? "icons/showing.png" : "icons/hidden.png";
+    const iconPath = message.showing
+      ? {
+          16: "icons/icon16.png",
+          32: "icons/icon32.png",
+          48: "icons/icon48.png",
+          128: "icons/icon128.png",
+        }
+      : {
+          16: "icons/closed/icon16.png",
+          32: "icons/closed/icon32.png",
+          48: "icons/closed/icon48.png",
+          128: "icons/closed/icon128.png",
+        };
+
+    if (sender.tab && sender.tab.id) {
+      chrome.action.setIcon({ path: iconPath, tabId: sender.tab.id });
+    } else {
+      // fallback: get active tab
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        if (tabs[0]) {
+          chrome.action.setIcon({ path: iconPath, tabId: tabs[0].id });
+        }
+      });
+    }
+  }
 });
--- src/content.js
+++ src/content.js
@@ -1,9 +1,20 @@
-document.querySelectorAll("input").forEach((input) => {
-	if (input.type === "password") {
-		input.dataset.originalType = "password";
-		input.type = "text";
-	} else if (input.dataset.originalType === "password") {
-		input.type = "password";
-		delete input.dataset.originalType;
-	}
-});
+(() => {
+  let showingPasswords = false;
+
+  document.querySelectorAll("input").forEach((input) => {
+    if (input.type === "password") {
+      input.dataset.originalType = "password";
+      input.type = "text";
+      showingPasswords = true;
+    } else if (input.dataset.originalType === "password") {
+      input.type = "password";
+      delete input.dataset.originalType;
+      showingPasswords = false;
+    }
+  });
+
+  chrome.runtime.sendMessage({
+    action: "updateIcon",
+    showing: showingPasswords,
+  });
+})();
